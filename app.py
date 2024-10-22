9fb518b08537c725c0dc2f0385dfd173b655fb3b
jayanth <jayanths8899@gmail.com>
2024-10-22 19:05:28 +0530

    fix

--- app.py
+++ app.py
@@ -110,5 +110,5 @@ def upload_file():
     return f"Infographic saved to: {infographic_path}", 200
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port, debug=True)
